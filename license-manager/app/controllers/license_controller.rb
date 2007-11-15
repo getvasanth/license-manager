eebbcd1febf2fd290285ee5a33530f196f12f69e
christophe.moine <christophe.moine@916a0078-a83d-0410-9f8b-8747f8e8042f>
2007-11-15 21:47:08 +0000

    Initial import.

--- license-manager/app/controllers/license_controller.rb
+++ license-manager/app/controllers/license_controller.rb
@@ -1,3 +1,13 @@
 class LicenseController < ApplicationController
   scaffold :license
+
+  def new
+    @license=License.new
+    @company=Company.find(params[:id])
+  end
+  
+  def create
+    @license = Company.find(params[:id]).licenses.create(params[:license])
+    redirect_to :controller => 'company', :action => 'show', :id => @license.company
+  end
 end

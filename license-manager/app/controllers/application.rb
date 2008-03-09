8a8a4d9d65dc1d4d1b92cf43e499e8603d8ba856
christophe.moine <christophe.moine@916a0078-a83d-0410-9f8b-8747f8e8042f>
2008-03-09 22:28:01 +0000

    login management added

--- license-manager/app/controllers/application.rb
+++ license-manager/app/controllers/application.rb
@@ -1,7 +1,11 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
+require_dependency "login_system"
 
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_LicenseManager_session_id'
+  
+  include LoginSystem
+  model :user
 end
